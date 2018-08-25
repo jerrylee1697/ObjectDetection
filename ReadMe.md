cfbe2ca31925738f04baa1f6656fe9b0160e2067
Jerry <jerrylee1697@gmail.com>
2018-08-24 23:56:21 -0700

    new files

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+Requirements that may be needed:
+pip install opencv-python
+sudo apt update && sudo apt install -y libsm6 libxext6 libxrender-dev
+
+Install VcXsrv Windows X Server for GUI in bash
+
+Command to run program for single image:
+python ProgramName.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel --image testimage.jpg
+
+Example:
+python deep_learning_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel --image images/example_01.jpg 
--- TestCamCapture.py
+++ TestCamCapture.py
@@ -3,18 +3,25 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
     ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # write the flipped frame
+        out.write(frame)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
-# When everything done, release the capture
+# Release everything if job is finished
 cap.release()
+out.release()
 cv2.destroyAllWindows()
